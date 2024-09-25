fbd8c43951849be298be1fe0473125dea776ba01
Sterphane Roque <stermiss2@gmail.com>
2024-09-24 23:14:30 -0300

    feat: validating the inputs

--- src/app/components/crud/crud.component.ts
+++ src/app/components/crud/crud.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-crud',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './crud.component.html',
   styleUrl: './crud.component.css',
 })
-export class CrudComponent {}
+export class CrudComponent {
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    age: new FormControl(null, [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(120),
+    ]),
+    city: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+}
