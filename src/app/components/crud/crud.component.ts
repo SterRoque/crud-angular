8296be2afc8a9cd8aaee1e0c0d456f2fe593feff
Sterphane Roque <stermiss2@gmail.com>
2024-09-24 22:12:30 -0300

    feat: create crud component

---
+++ src/app/components/crud/crud.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud.component.html',
+  styleUrl: './crud.component.css',
+})
+export class CrudComponent {}
