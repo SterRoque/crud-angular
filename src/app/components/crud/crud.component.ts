ca4628fb0b9110a6fbd1cee5ef48ac336325ff6f
Sterphane Roque <stermiss2@gmail.com>
2024-09-25 01:21:29 -0300

    fix: add condition to make the change, remove, and cancel button disappear

--- src/app/components/crud/crud.component.ts
+++ src/app/components/crud/crud.component.ts
@@ -1,26 +1,28 @@
 import { Component } from '@angular/core';
 import {
-   FormControl,
-   FormGroup,
-   ReactiveFormsModule,
-   Validators,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 
 @Component({
-   selector: 'app-crud',
-   standalone: true,
-   imports: [ReactiveFormsModule],
-   templateUrl: './crud.component.html',
-   styleUrl: './crud.component.css',
+  selector: 'app-crud',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './crud.component.html',
+  styleUrl: './crud.component.css',
 })
 export class CrudComponent {
-   form = new FormGroup({
-      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      age: new FormControl(null, [
-         Validators.required,
-         Validators.min(0),
-         Validators.max(120),
-      ]),
-      city: new FormControl('', [Validators.required, Validators.minLength(3)]),
-   });
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    age: new FormControl(null, [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(120),
+    ]),
+    city: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+
+  isVisibledRegisterButton: boolean = true;
 }
